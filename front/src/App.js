e96a8031d97d89ee60dbc1c769c638a7d94e9599
anamcb90 <amcorreab0619@gmail.com>
2022-10-15 15:11:24 -0500

    clase 13 octubre

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de mascostas Vety Shop</h1>
+      <img src="./images/logo.jpg" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
